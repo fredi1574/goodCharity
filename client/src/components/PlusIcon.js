2c92ba004cc1d10eaafacc98686f61910afc56e9
fredi1574 <fredi1574@gmail.com>
2023-12-24 18:19:44 +0200

    created a plus icon

---
+++ client/src/components/PlusIcon.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+
+import "../style/PlusIcon.css";
+
+function PlusIcon() {
+  return <FaPlus className="plus-icon" />;
+}
+
+export default PlusIcon;
