bc7fc35957cd96128e64ae5c47dbca8e87a32a1a
fredi1574 <fredi1574@gmail.com>
2023-12-24 18:22:16 +0200

    removed react boilerplate code and initiated own

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
+import Header from "./components/Header";
+import PlusIcon from "./components/PlusIcon";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="gradient">
+      <Header />
+      <PlusIcon />
+    </div>
+  );
 }
 
 export default App;
