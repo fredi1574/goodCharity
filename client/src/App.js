d124494cb58f9a4fced81ebe3ef95b225bf40afc
fredi1574 <fredi1574@gmail.com>
2023-12-25 19:26:29 +0200

    changed the web style

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,33 @@ import "./App.css";
 import Header from "./components/Header";
 import PlusIcon from "./components/PlusIcon";
 
+import Item from "./components/itemCard/Item";
+
 function App() {
   return (
-    <div className="gradient">
+    <div className="app">
       <Header />
+      <div className="itemList">
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </div>
       <PlusIcon />
     </div>
   );
