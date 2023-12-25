7380dde3bd0e0e8802e0f7f84c699269b7530810
fredi1574 <fredi1574@gmail.com>
2023-12-25 19:24:09 +0200

    added a header to the website

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,30 @@
 import React from "react";
 
+import "./style/Header.css";
+import { MdSunny, MdDarkMode } from "react-icons/md";
+
 function Header() {
-  return <div>Header</div>;
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  const userName = "John Doe";
+
+  // Function to toggle dark mode
+  const handleThemeClick = () => {
+    setDarkMode(!darkMode);
+    console.log("Dark mode toggled");
+  };
+
+  return (
+    <div className="header">
+      <div className="userName">{userName}</div>
+      <div className="logo">Good Charity</div>
+      {darkMode ? (
+        <MdDarkMode className="theme" onClick={handleThemeClick} />
+      ) : (
+        <MdSunny className="theme" onClick={handleThemeClick} />
+      )}
+    </div>
+  );
 }
 
 export default Header;
