b3309094b25d78cd2a066b2e2b363c8c5adcb9fb
fredi1574 <fredi1574@gmail.com>
2024-01-09 20:29:54 +0200

    Fetching the data from the database and showing it on the items grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,49 @@
+import { useState, useEffect } from "react";
+// import { Route, Routes, BrowserRouter } from "react-router-dom";
+
 import "./App.css";
 
+// Components
 import Header from "./components/header/Header";
 import PlusIcon from "./components/plusIcon/PlusIcon";
 import Item from "./components/itemCard/Item";
-import SignUp from "./components/signUp/SignUp";
+// import SignUp from "./components/signUp/SignUp";
 
 function App() {
-  //For testing purposes only.
-  //TODO: create a component that will render all the items
-  const items = [];
-  for (let i = 0; i < 30; i++) {
-    items.push(<Item key={i} />);
-  }
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    // Fetch items from API
+    const fetchItems = async () => {
+      try {
+        const response = await fetch("/api/items");
+        // Check whether the response status is 200-299
+        if (!response.ok) {
+          throw new Error(`An error has occured: ${response.status}`);
+        }
+
+        const jsonObjects = await response.json();
+        setItems(jsonObjects);
+        setIsLoading(false);
+        // Catch errors involving broader networking issues (not just response status)
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchItems();
+  }, []);
+
+  //TODO: create a container component that will render all the items
 
   return (
     <div className="app">
       <Header />
-      <div className="body">
-        <SignUp />
-        {/* <div className="itemList">{items}</div> */}
-      </div>
+      {/* <div className="body"><SignUp /></div> */}
+      {isLoading && <div className="loader">Loading...</div>}
+      {!isLoading && items.map((item) => <Item key={item._id} item={item} />)}
       <PlusIcon />
     </div>
   );
